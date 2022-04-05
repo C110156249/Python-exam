fe78e473e9cfd526804270ec27c98421b5fa1e63
unknown <C110156249@nkust.edu.tw>
2022-04-05 13:49:04 +0800

    第3題

---
+++ 3.py
@@ -0,0 +1,25 @@
+a = int(input("請輸入西元年"))
+if (a-1912)%12==0:
+    print("rat")
+elif(a-1912)%12==1:
+    print("ox")
+elif(a-1912)%12==2:
+    print("tiger")
+elif(a-1912)%12==3:
+    print("rabbit")
+elif(a-1912)%12==4:
+    print("dragon")
+elif(a-1912)%12==5:
+    print("snake")
+elif(a-1912)%12==6:
+    print("horse")
+elif(a-1912)%12==7:
+    print("sheep")
+elif(a-1912)%12==8:
+    print("monkey")
+elif(a-1912)%12==9:
+    print("rooster")
+elif(a-1912)%12==10:
+    print("dog")
+elif(a-1912)%12==11:
+    print("pig")
