137600a7e9406d850a57f2611846f642bb5abbbe
unknown <C110156249@nkust.edu.tw>
2022-04-05 15:02:17 +0800

    第11題

---
+++ 11.py
@@ -0,0 +1,25 @@
+a = int(input("輸入月及日為:"))
+if 121<=a and a<=218:
+    print("星座為:Aquarius")
+elif 219<=a and a<=320:
+    print("星座為:Pisces")
+elif 321<=a and a<=420:
+    print("星座為:Aries")
+elif 421<=a and a<=521:
+    print("星座為:Taurus")
+elif 522<=a and a<=621:
+    print("星座為:Gemini")
+elif 622<=a and a<=722:
+    print("星座為:Cancer")
+elif 723<=a and a<=823:
+    print("星座為:Leo")
+elif 824<=a and a<=923:
+    print("星座為:Virgo")
+elif 924<=a and a<=1023:
+    print("星座為:Libra")
+elif 1024<=a and a<=1122:
+    print("星座為:Scorpio")
+elif 1123<=a and a<=1221:
+    print("星座為:Sagittarius")
+elif 1222<=a or a<=120:
+    print("星座為:Capricorn")
