f35eb03619c061a0f08cbcbe5fd5315368719e46
unknown <C110156249@nkust.edu.tw>
2022-04-16 23:56:47 +0800

    第42題

---
+++ 42.py
@@ -0,0 +1,15 @@
+from traceback import print_tb
+
+
+a =int(input("a:"))
+b =int(input("b:"))
+c =int(input("c:"))
+d = (((-b)+((b*b)-(4*a*c))**0.5)/(2*a))
+e = (((-b)-((b*b)-(4*a*c))**0.5)/(2*a))
+if (b*b)-(4*a*c) < 0:
+    print("無解")
+elif (b*b)-(4*a*c) == 0:
+    print("%.0f"%d)
+else:
+    print("%.0f"%d)
+    print("%.0f"%e)
