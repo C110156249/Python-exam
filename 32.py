37d63a6c1b49e6e636d07b42dfc39dca492434fc
unknown <C110156249@nkust.edu.tw>
2022-04-16 23:08:55 +0800

    第32題

---
+++ 32.py
@@ -0,0 +1,11 @@
+from code import InteractiveInterpreter
+
+
+m = int(input("小明身上有幾元:"))
+n = int(input("販賣機有幾種飲料:"))
+a = 0
+for i in range(n):
+    p = int(input("飲料價格:"))
+    if m >= p:
+        a = a + 1
+print("小明可以購買",a,"種飲料")
