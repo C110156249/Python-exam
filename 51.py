79bbcb6e285c5f4f25f72778053628c36efb19bf
unknown <C110156249@nkust.edu.tw>
2022-04-17 21:25:58 +0800

    第51題

---
+++ 51.py
@@ -0,0 +1,13 @@
+from gettext import find
+
+
+a = list(input("輸入自傳(至少10個字):"))
+a.remove("，")
+a.remove("。")
+d = []
+for i in range(len(a)):
+    b = a.count(a[i])
+    c = d.count(a[i])
+    if b > 1 and c == 0:
+        d.append(a[i])
+print(d)
